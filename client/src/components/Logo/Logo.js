c31b1e70efa50df167d91e95e7b494b8ba35e6fa
BrandonWade <brandon_wade2005@hotmail.com>
2020-09-23 23:30:18 -0500

    Begin adding support for mobile.

--- client/src/components/Logo/Logo.js
+++ client/src/components/Logo/Logo.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Logo.scss';
 
-const Logo = ({ size = 'small' }) => {
-    const className = size === 'small' ? 'Logo--small' : 'Logo--large';
+const Logo = ({ size = 'small', className = '' }) => {
+    const logoSize = size === 'small' ? 'Logo--small' : 'Logo--large';
     return (
-        <span className={`Logo ${className}`}>
+        <span className={`Logo ${logoSize} ${className}`}>
             <Link to='/'>Blackblade</Link>
         </span>
     );
