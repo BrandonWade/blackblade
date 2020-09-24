426159c2103e8c7725117b19aa43157d674a98c5
BrandonWade <brandon_wade2005@hotmail.com>
2020-09-23 23:30:18 -0500

    Begin adding support for mobile.

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -27,7 +27,7 @@ const Header = () => {
     return (
         <div className='Header'>
             <div className='Header-content'>
-                <Logo />
+                <Logo className='Header-logo' />
                 <form className='Header-searchForm' onSubmit={onSubmit}>
                     <Input placeholder='Search' className='Header-searchBox' value={query} onChange={onChange} />
                 </form>
