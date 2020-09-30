901acb2d4fe0a5fad42f7130c23799fc659a90b1
BrandonWade <brandon_wade2005@hotmail.com>
2020-09-30 18:59:34 -0500

    Display all faces per card row in deck builder.

--- client/src/components/DeckList/DeckRow.js
+++ client/src/components/DeckList/DeckRow.js
@@ -1,11 +1,10 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import useSymbols from '../../hooks/useSymbols';
+import DeckRowManaCost from './DeckRowManaCost';
 import Input from '../../components/Input';
 
 const DeckRow = ({ card = {}, updateCount = () => {}, removeCard = () => {} }) => {
-    const cardFace = card?.sets_json?.[0]?.card_faces?.[0];
-    const manaCost = useSymbols(cardFace?.mana_cost || '');
+    const cardFaces = card?.sets_json?.[0]?.card_faces;
     const [count, setCount] = useState(card.count);
 
     const onCountChange = e => {
@@ -22,9 +21,17 @@ const DeckRow = ({ card = {}, updateCount = () => {}, removeCard = () => {} }) =
             <td className='DeckTable-count'>
                 <Input className='DeckTable-input' value={count} onChange={onCountChange} />
             </td>
-            <td className='DeckTable-manaCost' dangerouslySetInnerHTML={{ __html: manaCost }} />
-            <td className='DeckTable-name'>
-                <Link to={`/cards/${card.card_id}`}>{cardFace?.name}</Link>
+            <td className='DeckTable-manaCosts'>
+                {cardFaces.map((face, i) => (
+                    <DeckRowManaCost key={i} manaCost={face.mana_cost} />
+                ))}
+            </td>
+            <td className='DeckTable-names'>
+                {cardFaces.map((face, i) => (
+                    <div key={i} className='DeckTable-subRow'>
+                        <Link to={`/cards/${card.card_id}`}>{face.name}</Link>
+                    </div>
+                ))}
             </td>
             <td className='DeckTable-remove' onClick={onRemoveCard}>
                 &#10799;
