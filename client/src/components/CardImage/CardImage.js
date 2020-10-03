1d2d6238646318bb282f7e2a8484150a23b48373
BrandonWade <brandon_wade2005@hotmail.com>
2020-10-03 00:03:39 -0500

    Continue implementing card page layout transforms.

--- client/src/components/CardImage/CardImage.js
+++ client/src/components/CardImage/CardImage.js
@@ -1,18 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Button from '../Button';
 import './CardImage.scss';
 
 const CardImage = ({ cardFaces = [], layout = '' }) => {
     const [flipped, setFlipped] = useState(false);
-    const [selectedFaceIndex, setSelectedFaceIndex] = useState(0);
-    const image = cardFaces?.[selectedFaceIndex]?.image;
-    const name = cardFaces?.[selectedFaceIndex]?.name;
+    const [rotated, setRotated] = useState(false);
+    const [transformed, setTransformed] = useState(false);
+    const image = cardFaces?.[0]?.image;
+    const name = cardFaces?.[0]?.name;
+    const classNameFront = `${flipped ? 'CardImage-imageFront--flipped' : ''} ${rotated ? 'CardImage-imageFront--rotated' : ''} ${
+        transformed ? 'CardImage-imageFront--transformed' : ''
+    }`;
+    const classNameBack = `${transformed ? 'CardImage-imageBack--transformed' : ''}`;
+    const canFlip = layout === 'flip';
+    const canRotate = layout === 'split';
+    const canTransform = layout === 'transform' || layout === 'double_faced_token';
+
+    useEffect(() => {
+        setFlipped(false);
+        setRotated(false);
+        setTransformed(false);
+    }, [cardFaces]);
 
     const renderButton = () => {
-        if (['flip'].includes(layout)) {
+        if (canFlip) {
             return (
-                <Button className='CardImage-transformButton' onClick={() => setFlipped(!flipped)}>
+                <Button className='CardImage-button' onClick={() => setFlipped(!flipped)}>
                     Flip
+                </Button>
+            );
+        } else if (canRotate) {
+            return (
+                <Button className='CardImage-button' onClick={() => setRotated(!rotated)}>
+                    Rotate
+                </Button>
+            );
+        } else if (canTransform) {
+            return (
+                <Button className='CardImage-button' onClick={() => setTransformed(!transformed)}>
+                    Transform
                 </Button>
             );
         }
@@ -22,7 +48,8 @@ const CardImage = ({ cardFaces = [], layout = '' }) => {
 
     return (
         <div className='CardImage'>
-            <img className={`CardImage-image ${flipped ? 'CardImage-image--flipped' : ''}`} src={image} alt={name} />
+            <img className={`CardImage-imageFront ${classNameFront}`} src={image} alt={name} />
+            {canTransform && <img className={`CardImage-imageBack ${classNameBack}`} src={cardFaces?.[1]?.image} alt={name} />}
             {renderButton()}
         </div>
     );
