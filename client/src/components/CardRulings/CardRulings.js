690e72cd6163982935efde557216660a737e8ed1
BrandonWade <brandon_wade2005@hotmail.com>
2020-11-04 19:54:14 -0600

    Fix bug where card rulings and layout button may not display.

--- client/src/components/CardRulings/CardRulings.js
+++ client/src/components/CardRulings/CardRulings.js
@@ -2,28 +2,21 @@ import React from 'react';
 import CardSection from '../CardSection';
 import './CardRulings.scss';
 
-const CardRulings = (props) => {
+const CardRulings = props => {
     const rulings = props.rulings || [];
-    return (
-        (rulings || []).length > 0 ? (
-          <div className='CardRulings'>
-              {
-                  (rulings || []).map(ruling => {
-                      return (
-                          <CardSection key={ruling.id} className='CardRulings-ruling'>
-                              <div className='CardSection-rowItem CardRulings-comment'>
-                                  {ruling.comment}
-                              </div>
-                              <div className='CardSection-rowItem CardRulings-publishedAt'>
-                                  {ruling.published_at}
-                              </div>
-                          </CardSection>
-                      );
-                  })
-              }
-          </div>
-        ) : null
-    )
-}
+
+    return rulings.length > 0 ? (
+        <div className='CardRulings'>
+            {rulings.map(ruling => {
+                return (
+                    <CardSection key={ruling.id} className='CardRulings-ruling'>
+                        <div className='CardSection-rowItem CardRulings-comment'>{ruling.comment}</div>
+                        <div className='CardSection-rowItem CardRulings-publishedAt'>{ruling.published_at}</div>
+                    </CardSection>
+                );
+            })}
+        </div>
+    ) : null;
+};
 
 export default CardRulings;
