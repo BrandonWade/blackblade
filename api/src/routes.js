af94dc13bbeee3068201ccca5a233522d2c34a99
BrandonWade <brandon_wade2005@hotmail.com>
2020-11-06 22:30:41 -0600

    Add backend for random card.

--- api/src/routes.js
+++ api/src/routes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { searchValidators, cardValidators } from './validators/search';
-import { search, getCardByID } from './controllers/search';
+import { search, getCardByID, getRandomCard } from './controllers/search';
 import { createDeck, saveDeck, getDeck } from './controllers/decks';
 
 const router = express.Router();
 
 router.get('/search', searchValidators, search);
+router.get('/cards/random', getRandomCard);
 router.get('/cards/:id', cardValidators, getCardByID);
 router.post('/decks', createDeck);
 router.put('/decks/:publicID', saveDeck);
