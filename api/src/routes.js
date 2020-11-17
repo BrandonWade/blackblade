da3f77186fb2ebd11fe62f2bfa161fbd361e1143
BrandonWade <brandon_wade2005@hotmail.com>
2020-11-16 21:04:04 -0600

    Move validation error response to middleware.

--- api/src/routes.js
+++ api/src/routes.js
@@ -2,12 +2,13 @@ import express from 'express';
 import { searchValidators, cardValidators } from './validators/search';
 import { search, getCardByID, getRandomCard } from './controllers/search';
 import { createDeck, saveDeck, getDeck } from './controllers/decks';
+import validate from './middleware/validate';
 
 const router = express.Router();
 
-router.get('/search', searchValidators, search);
+router.get('/search', searchValidators, validate, search);
 router.get('/cards/random', getRandomCard);
-router.get('/cards/:id', cardValidators, getCardByID);
+router.get('/cards/:id', cardValidators, validate, getCardByID);
 router.post('/decks', createDeck);
 router.put('/decks/:publicID', saveDeck);
 router.get('/decks/:publicID', getDeck);
