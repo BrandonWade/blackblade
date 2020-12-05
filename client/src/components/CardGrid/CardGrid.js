8d320ee9947ec4f35ef8b9435e587c4bd7969409
BrandonWade <brandon_wade2005@hotmail.com>
2020-12-05 01:49:36 -0600

    Remove debugging code.

--- client/src/components/CardGrid/CardGrid.js
+++ client/src/components/CardGrid/CardGrid.js
@@ -3,12 +3,11 @@ import NoResults from '../NoResults';
 import './CardGrid.scss';
 
 const CardGrid = ({ cards = [], onClick = () => {} }) => {
-    console.log('@@@@@', cards);
     return (
         <NoResults showMessage={cards.length === 0}>
             <div className='CardGrid'>
                 {cards.map(card => {
-                    const cardFace = card?.sets_json?.[0]?.card_faces?.[0];
+                    const cardFace = card?.sets_json?.[0]?.card_faces?.[0]; // TODO: Assumes a list of cards is passed in instead of a list of "card shaped images"
                     return (
                         <img
                             key={card.card_id}
