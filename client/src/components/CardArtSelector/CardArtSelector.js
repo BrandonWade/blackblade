bb19057c2c8e1f4361eb5e201e701994f366bdf6
BrandonWade <brandon_wade2005@hotmail.com>
2020-12-08 20:36:32 -0600

    Add logic for selecting a card variant in the card art selector.

--- client/src/components/CardArtSelector/CardArtSelector.js
+++ client/src/components/CardArtSelector/CardArtSelector.js
@@ -1,19 +1,33 @@
 import React, { useContext } from 'react';
 import CardArtSelectorContext from '../../contexts/CardArtSelectorContext';
 import CardContext from '../../contexts/CardContext';
+import DeckBuilderContext from '../../contexts/DeckBuilderContext';
 import Overlay from '../Overlay';
 import CardGrid from '../CardGrid';
 import './CardArtSelector.scss';
 
 const CardArtSelector = () => {
     const { artSelectorVisible, setArtSelectorVisible } = useContext(CardArtSelectorContext);
-    const { card } = useContext(CardContext);
+    const { card, setCard } = useContext(CardContext);
+    const { deckCards, setDeckCards } = useContext(DeckBuilderContext);
     const sets = card.sets_json || [];
+
+    const onSelectCard = cardVariant => {
+        const index = deckCards.findIndex(c => c.card_id === card.card_id);
+        const updatedCard = {
+            ...deckCards[index],
+            ...cardVariant,
+        };
+        const cards = [...deckCards.slice(0, index), updatedCard, ...deckCards.slice(index + 1)];
+        setCard(updatedCard);
+        setDeckCards(cards);
+        setArtSelectorVisible(false);
+    };
 
     return artSelectorVisible ? (
         <div className='CardArtSelector'>
             <Overlay className='CardArtSelector-content' visible={true} setVisible={setArtSelectorVisible}>
-                <CardGrid className='CardArtSelector-cardGrid' cards={sets} onClick={() => console.log('TODO: Implement')} />
+                <CardGrid className='CardArtSelector-cardGrid' cards={sets} onClick={onSelectCard} />
             </Overlay>
         </div>
     ) : null;
