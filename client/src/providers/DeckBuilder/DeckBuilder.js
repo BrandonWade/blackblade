419625eb854f46102d5120f9fe9e5807fac1a8b2
BrandonWade <brandon_wade2005@hotmail.com>
2020-12-26 07:07:35 -0600

    Use action constants in providers.

--- client/src/providers/DeckBuilder/DeckBuilder.js
+++ client/src/providers/DeckBuilder/DeckBuilder.js
@@ -1,17 +1,26 @@
 import React, { useReducer } from 'react';
 import DeckBuilderReducer from '../../reducers/DeckBuilder';
 import DeckBuilderContext, { initialState } from '../../contexts/DeckBuilder';
+import {
+    SET_DECK_NAME,
+    SET_DECK_CARDS,
+    SET_MAYBEBOARD_CARDS,
+    SET_UNMODIFIED_DECK_NAME,
+    SET_UNMODIFIED_DECK_CARDS,
+    SET_UNMODIFIED_MAYBEBOARD_CARDS,
+    SET_MAYBEBOARD_MODE,
+} from '../../actions/DeckBuilder';
 
 function DeckBuilderProvider({ children = [] }) {
     const [state, dispatch] = useReducer(DeckBuilderReducer, initialState);
 
-    const setDeckName = deckName => dispatch({ type: 'SET_DECK_NAME', deckName });
-    const setDeckCards = deckCards => dispatch({ type: 'SET_DECK_CARDS', deckCards });
-    const setMaybeboardCards = maybeboardCards => dispatch({ type: 'SET_MAYBEBOARD_CARDS', maybeboardCards });
-    const setUnmodifiedDeckName = unmodifiedDeckName => dispatch({ type: 'SET_UNMODIFIED_DECK_NAME', unmodifiedDeckName });
-    const setUnmodifiedDeckCards = unmodifiedDeckCards => dispatch({ type: 'SET_UNMODIFIED_DECK_CARDS', unmodifiedDeckCards });
-    const setUnmodifiedMaybeboardCards = unmodifiedMaybeboardCards => dispatch({ type: 'SET_UNMODIFIED_MAYBEBOARD_CARDS', unmodifiedMaybeboardCards });
-    const setMaybeboardMode = maybeboardMode => dispatch({ type: 'SET_MAYBEBOARD_MODE', maybeboardMode });
+    const setDeckName = deckName => dispatch({ type: SET_DECK_NAME, deckName });
+    const setDeckCards = deckCards => dispatch({ type: SET_DECK_CARDS, deckCards });
+    const setMaybeboardCards = maybeboardCards => dispatch({ type: SET_MAYBEBOARD_CARDS, maybeboardCards });
+    const setUnmodifiedDeckName = unmodifiedDeckName => dispatch({ type: SET_UNMODIFIED_DECK_NAME, unmodifiedDeckName });
+    const setUnmodifiedDeckCards = unmodifiedDeckCards => dispatch({ type: SET_UNMODIFIED_DECK_CARDS, unmodifiedDeckCards });
+    const setUnmodifiedMaybeboardCards = unmodifiedMaybeboardCards => dispatch({ type: SET_UNMODIFIED_MAYBEBOARD_CARDS, unmodifiedMaybeboardCards });
+    const setMaybeboardMode = maybeboardMode => dispatch({ type: SET_MAYBEBOARD_MODE, maybeboardMode });
 
     const props = {
         ...state,
--- client/src/providers/Search/Search.js
+++ client/src/providers/Search/Search.js
@@ -1,19 +1,20 @@
 import React, { useReducer } from 'react';
 import SearchReducer from '../../reducers/Search';
 import SearchContext, { initialState } from '../../contexts/Search';
+import { SET_NAME, SET_TEXT, SET_TYPE, SET_COLORS, SET_SET, SET_PAGE, SET_TOTAL_RESULTS, SET_SEARCH_RESULTS, SET_NUMBER_OF_PAGES } from '../../actions/Search';
 
 function SearchProvider({ children = [] }) {
     const [state, dispatch] = useReducer(SearchReducer, initialState);
 
-    const setName = name => dispatch({ type: 'SET_NAME', name });
-    const setText = text => dispatch({ type: 'SET_TEXT', text });
-    const setType = type => dispatch({ type: 'SET_TYPE', cardType: type });
-    const setColors = (color, value) => dispatch({ type: 'SET_COLORS', color, value });
-    const setSet = set => dispatch({ type: 'SET_SET', set });
-    const setPage = page => dispatch({ type: 'SET_PAGE', page });
-    const setTotalResults = totalResults => dispatch({ type: 'SET_TOTAL_RESULTS', totalResults });
-    const setSearchResults = searchResults => dispatch({ type: 'SET_SEARCH_RESULTS', searchResults });
-    const setNumberOfPages = numberOfPages => dispatch({ type: 'SET_NUMBER_OF_PAGES', numberOfPages });
+    const setName = name => dispatch({ type: SET_NAME, name });
+    const setText = text => dispatch({ type: SET_TEXT, text });
+    const setType = type => dispatch({ type: SET_TYPE, cardType: type });
+    const setColors = (color, value) => dispatch({ type: SET_COLORS, color, value });
+    const setSet = set => dispatch({ type: SET_SET, set });
+    const setPage = page => dispatch({ type: SET_PAGE, page });
+    const setTotalResults = totalResults => dispatch({ type: SET_TOTAL_RESULTS, totalResults });
+    const setSearchResults = searchResults => dispatch({ type: SET_SEARCH_RESULTS, searchResults });
+    const setNumberOfPages = numberOfPages => dispatch({ type: SET_NUMBER_OF_PAGES, numberOfPages });
 
     const props = {
         ...state,
