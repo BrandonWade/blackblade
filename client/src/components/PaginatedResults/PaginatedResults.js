9842416aed034877bc78310c2a8de951f7c49960
BrandonWade <brandon_wade2005@hotmail.com>
2021-01-13 04:05:21 -0600

    Fix bug where pagination was broken when searching by set or type.

--- client/src/components/PaginatedResults/PaginatedResults.js
+++ client/src/components/PaginatedResults/PaginatedResults.js
@@ -7,15 +7,42 @@ import CardGrid from '../CardGrid';
 import './PaginatedResults.scss';
 
 function PaginatedResults({ className = '', onSelectResult = () => {}, redirect = true }) {
-    const { name, text, types, colors, matchType, set, cmc, power, toughness, loyalty, rarities, flavorText, searchResults, setPage } = useContext(
-        SearchContext
-    );
+    const {
+        name,
+        text,
+        selectedTypes,
+        colors,
+        matchType,
+        selectedSets,
+        cmc,
+        power,
+        toughness,
+        loyalty,
+        rarities,
+        flavorText,
+        searchResults,
+        setPage,
+    } = useContext(SearchContext);
     const { searchCards } = useSearch();
     const { searchResultsRedirect, displayResults } = useDisplayResults();
 
     const fetchResults = async (page = 1) => {
         if (redirect === true) {
-            searchResultsRedirect({ name, text, types, colors, matchType, set, cmc, power, toughness, loyalty, rarities, flavorText, page });
+            searchResultsRedirect({
+                name,
+                text,
+                selectedTypes,
+                colors,
+                matchType,
+                selectedSets,
+                cmc,
+                power,
+                toughness,
+                loyalty,
+                rarities,
+                flavorText,
+                page,
+            });
         } else {
             const response = await searchCards({ name, page });
             displayResults(response);
