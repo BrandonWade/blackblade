524b6d38cf588f725fff44e4bb785de4fda88d67
BrandonWade <brandon_wade2005@hotmail.com>
2021-01-13 04:04:54 -0600

    Move set and type logic into separate components.

--- client/src/pages/AdvancedSearch/AdvancedSearch.js
+++ client/src/pages/AdvancedSearch/AdvancedSearch.js
@@ -1,13 +1,11 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext } from 'react';
 import useDisplayResults from '../../hooks/useDisplayResults';
-import useFetchCardTypes from '../../hooks/useFetchCardTypes';
-import useFetchCardSets from '../../hooks/useFetchCardSets';
-import AdvancedSearchContext from '../../contexts/AdvancedSearch';
 import SearchContext from '../../contexts/Search';
 import HeaderPage from '../../components/HeaderPage';
 import { InputField } from '../../components/Input';
-import { MultiSelectField } from '../../components/Select';
 import Button from '../../components/Button';
+import CardTypes from './CardTypes';
+import CardSets from './CardSets';
 import ColorOptions from './ColorOptions';
 import RarityOptions from './RarityOptions';
 import StatRow from './StatRow';
@@ -15,22 +13,15 @@ import './AdvancedSearch.scss';
 
 function AdvancedSearch() {
     const { searchResultsRedirect } = useDisplayResults();
-    const { getCardTypes } = useFetchCardTypes();
-    const { getCardSets } = useFetchCardSets();
-    const { cardTypes, cardSets } = useContext(AdvancedSearchContext);
     const {
         name,
         setName,
         text,
         setText,
         selectedTypes,
-        addType,
-        removeType,
         colors,
         matchType,
         selectedSets,
-        addSet,
-        removeSet,
         cmc,
         power,
         toughness,
@@ -41,33 +32,12 @@ function AdvancedSearch() {
         setFlavorText,
     } = useContext(SearchContext);
 
-    useEffect(() => {
-        getCardTypes();
-        getCardSets();
-    }, [getCardTypes, getCardSets]);
-
     const onChangeName = e => {
         setName(e.target.value);
     };
 
     const onChangeText = e => {
         setText(e.target.value);
-    };
-
-    const onSelectType = e => {
-        addType(e.target.value);
-    };
-
-    const onClearType = type => {
-        removeType(type);
-    };
-
-    const onSelectSet = e => {
-        addSet(e.target.value);
-    };
-
-    const onClearSet = setCode => {
-        removeSet(setCode);
     };
 
     const onChangeFlavorText = e => {
@@ -94,68 +64,6 @@ function AdvancedSearch() {
         });
     };
 
-    const getFormattedSelectedTypes = () => {
-        return selectedTypes.reduce((types, cardType) => {
-            const type = cardTypes.find(t => t.type === cardType);
-
-            if (type) {
-                return types.concat({
-                    value: cardType,
-                    text: cardType,
-                });
-            }
-
-            return types;
-        }, []);
-    };
-
-    const getFormattedSelectedSets = () => {
-        return selectedSets.reduce((sets, setCode) => {
-            const set = cardSets.find(s => s.set_code === setCode);
-
-            if (set) {
-                return sets.concat({
-                    value: setCode,
-                    text: set.set_name,
-                });
-            }
-
-            return sets;
-        }, []);
-    };
-
-    const renderFilteredTypes = () => {
-        const setOfSelectedTypes = new Set(selectedTypes);
-        const filteredTypes = cardTypes.filter(t => !setOfSelectedTypes.has(t.type));
-
-        return (
-            <>
-                <option value=''>Choose a card type</option>
-                {filteredTypes.map(t => (
-                    <option key={t.id} value={t.type}>
-                        {t.type}
-                    </option>
-                ))}
-            </>
-        );
-    };
-
-    const renderFilteredSets = () => {
-        const setOfSelectedSets = new Set(selectedSets);
-        const filteredSets = cardSets.filter(s => !setOfSelectedSets.has(s.set_code));
-
-        return (
-            <>
-                <option value=''>Choose a card set</option>
-                {filteredSets.map(s => (
-                    <option key={s.id} value={s.set_code}>
-                        {s.set_name}
-                    </option>
-                ))}
-            </>
-        );
-    };
-
     return (
         <HeaderPage className='AdvancedSearch'>
             <div className='AdvancedSearch-content'>
@@ -174,27 +82,9 @@ function AdvancedSearch() {
                         value={text}
                         onChange={onChangeText}
                     />
-                    <MultiSelectField
-                        labelClassName='AdvancedSearch-label'
-                        label='Types'
-                        className='AdvancedSearch-select'
-                        selectedOptions={getFormattedSelectedTypes()}
-                        onSelectOption={onSelectType}
-                        onClearOption={onClearType}
-                    >
-                        {renderFilteredTypes()}
-                    </MultiSelectField>
+                    <CardTypes />
                     <ColorOptions labelClassName='AdvancedSearch-label' label='Colors' />
-                    <MultiSelectField
-                        labelClassName='AdvancedSearch-label'
-                        label='Sets'
-                        className='AdvancedSearch-select'
-                        selectedOptions={getFormattedSelectedSets()}
-                        onSelectOption={onSelectSet}
-                        onClearOption={onClearSet}
-                    >
-                        {renderFilteredSets()}
-                    </MultiSelectField>
+                    <CardSets />
                     <StatRow
                         labelClassName='AdvancedSearch-label'
                         label='CMC'
---
+++ client/src/pages/AdvancedSearch/CardSets.js
@@ -0,0 +1,69 @@
+import React, { useEffect, useContext } from 'react';
+import useFetchCardSets from '../../hooks/useFetchCardSets';
+import AdvancedSearchContext from '../../contexts/AdvancedSearch';
+import SearchContext from '../../contexts/Search';
+import { MultiSelectField } from '../../components/Select';
+
+function CardTypes() {
+    const { getCardSets } = useFetchCardSets();
+    const { cardSets } = useContext(AdvancedSearchContext);
+    const { selectedSets, addSet, removeSet } = useContext(SearchContext);
+
+    useEffect(() => {
+        getCardSets();
+    }, [getCardSets]);
+
+    const onSelectSet = e => {
+        addSet(e.target.value);
+    };
+
+    const onClearSet = setCode => {
+        removeSet(setCode);
+    };
+
+    const getFormattedSelectedSets = () => {
+        return selectedSets.reduce((sets, setCode) => {
+            const set = cardSets.find(s => s.set_code === setCode);
+
+            if (set) {
+                return sets.concat({
+                    value: setCode,
+                    text: set.set_name,
+                });
+            }
+
+            return sets;
+        }, []);
+    };
+
+    const renderFilteredSets = () => {
+        const setOfSelectedSets = new Set(selectedSets);
+        const filteredSets = cardSets.filter(s => !setOfSelectedSets.has(s.set_code));
+
+        return (
+            <>
+                <option value=''>Choose a card set</option>
+                {filteredSets.map(s => (
+                    <option key={s.id} value={s.set_code}>
+                        {s.set_name}
+                    </option>
+                ))}
+            </>
+        );
+    };
+
+    return (
+        <MultiSelectField
+            labelClassName='AdvancedSearch-label'
+            label='Sets'
+            className='AdvancedSearch-select'
+            selectedOptions={getFormattedSelectedSets()}
+            onSelectOption={onSelectSet}
+            onClearOption={onClearSet}
+        >
+            {renderFilteredSets()}
+        </MultiSelectField>
+    );
+}
+
+export default CardTypes;
