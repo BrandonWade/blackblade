250f1f5d20b5b139fa91181208280d6ae9e2cbe2
BrandonWade <brandon_wade2005@hotmail.com>
2021-01-14 20:33:44 -0600

    Add custom NumberInput component.
    - Fix bug where StatRow fields values couldn't be used for searching.

---
+++ client/src/components/NumberInput/NumberInput.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import withFormField from '../../hocs/withFormField';
+import { ChevronThinDown, ChevronThinUp } from '../Icons';
+import './NumberInput.scss';
+
+function NumberInput({
+    className = '',
+    placeholder = '',
+    name = '',
+    value = 0,
+    onChange = () => {},
+    onIncrement = () => {},
+    onDecrement = () => {},
+}) {
+    return (
+        <span className='NumberInput-wrapper'>
+            <input type='number' className={`NumberInput ${className}`} placeholder={placeholder} name={name} value={value} onChange={onChange} />
+            <span className='NumberInput-buttons'>
+                <span className='NumberInput-buttonWrapper NumberInput-increment' onClick={onIncrement}>
+                    <ChevronThinUp />
+                </span>
+                <span className='NumberInput-buttonWrapper NumberInput-decrement' onClick={onDecrement}>
+                    <ChevronThinDown />
+                </span>
+            </span>
+        </span>
+    );
+}
+
+export default NumberInput;
+
+export const NumberInputField = withFormField(NumberInput);
---
+++ client/src/components/NumberInput/index.js
@@ -0,0 +1,1 @@
+export { default, NumberInputField } from './NumberInput';
