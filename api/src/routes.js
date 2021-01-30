8518df5310bc67b693d21738ee551b457989ef8c
BrandonWade <brandon_wade2005@hotmail.com>
2021-01-30 04:36:32 -0600

    Begin setting up account register logic.

--- api/src/routes.js
+++ api/src/routes.js
@@ -8,6 +8,7 @@ import {
     getCardSets,
 } from './controllers/search';
 import { createDeck, saveDeck, getDeck } from './controllers/decks';
+import { registerAccount } from './controllers/accounts';
 import validate from './middleware/validate';
 
 const router = express.Router();
@@ -20,5 +21,6 @@ router.get('/sets', getCardSets);
 router.post('/decks', createDeck);
 router.put('/decks/:publicID', saveDeck);
 router.get('/decks/:publicID', getDeck);
+router.post('/accounts/register', registerAccount);
 
 export default router;
