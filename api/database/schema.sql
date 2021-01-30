13c37d26fc833de56fc055631792870ed341baba
BrandonWade <brandon_wade2005@hotmail.com>
2021-01-29 22:08:44 -0600

    Add accounts schema.

--- api/database/schema.sql
+++ api/database/schema.sql
@@ -178,3 +178,16 @@ CREATE TABLE types (
     PRIMARY KEY (id),
     UNIQUE KEY U_type (type)
 ) CHARSET=utf8mb4;
+
+DROP TABLE IF EXISTS accounts;
+CREATE TABLE accounts (
+  id int unsigned NOT NULL AUTO_INCREMENT,
+  email_hash varbinary(256) NOT NULL DEFAULT '',
+  email_enc varbinary(300) NOT NULL DEFAULT '',
+  password_hash varbinary(64) NOT NULL DEFAULT '',
+  is_activated tinyint(1) NOT NULL DEFAULT 0,
+  created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (id),
+  UNIQUE KEY U_email_hash (email_hash)
+) CHARSET=utf8mb4;
