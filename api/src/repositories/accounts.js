99a6c08d90a9ed9dc887c088df2e09ec292c24f9
BrandonWade <brandon_wade2005@hotmail.com>
2021-01-31 16:54:29 -0600

    Add logic for hashing emails and passwords on account register.

--- api/src/repositories/accounts.js
+++ api/src/repositories/accounts.js
@@ -2,17 +2,18 @@ import { connection } from '../db';
 
 const registerAccount = async (emailIV, emailEnc, emailHash, passwordHash) => {
     return connection.query(
-        `INSERT INTO accounts(
-        email_iv,
-        email_enc,
-        email_hash,
-        password_hash
-    ) VALUES (
-        ?,
-        ?,
-        ?,
-        ?
-    )`,
+        `INSERT INTO accounts (
+            email_iv,
+            email_enc,
+            email_hash,
+            password_hash
+        ) VALUES (
+            ?,
+            ?,
+            ?,
+            ?
+        )
+    `,
         [emailIV, emailEnc, emailHash, passwordHash],
     );
 };
--- api/src/services/accounts.js
+++ api/src/services/accounts.js
@@ -1,11 +1,12 @@
+import hashValue from '../helpers/hash';
 import AccountRepository from '../repositories/accounts';
 
 // TODO: Implement
 const registerAccount = async (email, password) => {
     const emailIV = '';
     const emailEnc = '';
-    const emailHash = '';
-    const passwordHash = '';
+    const emailHash = await hashValue(email);
+    const passwordHash = await hashValue(password);
 
     return AccountRepository.registerAccount(
         emailIV,
