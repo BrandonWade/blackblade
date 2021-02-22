501497c61e2d4eee6c9d5928e0977fa92284a073
BrandonWade <brandon_wade2005@hotmail.com>
2021-02-22 00:02:59 -0600

    Use custom error types when an error occurs.

--- api/src/clients/mailjet.js
+++ api/src/clients/mailjet.js
@@ -1,20 +1,19 @@
-import { StatusCodes } from 'http-status-codes';
 import * as mailjet from 'node-mailjet';
 
-const send = async (message) => {
+const send = async (email, message) => {
     const client = mailjet.connect(
         process.env.MAILJET_PUBLIC_KEY,
         process.env.MAILJET_PRIVATE_KEY,
     );
 
-    const { response } = await client
-        .post('send', { version: 'v3.1' })
-        .request({
+    try {
+        await client.post('send', { version: 'v3.1' }).request({
             Messages: [message],
         });
-    if (response.statusCode !== StatusCodes.OK) {
-        console.error('error contacting mailjet', response.statusCode);
-        return false;
+    } catch (e) {
+        const statusCode = e?.response?.statusCode || 0;
+        const text = e?.response?.error?.text || '';
+        throw `error occurred while sending email to ${email} with status ${statusCode} and text ${text}`;
     }
 
     return true;
