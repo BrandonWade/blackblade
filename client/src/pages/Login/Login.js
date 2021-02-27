bb6cd6a99e27cc60df91f27f7bfc7cec59c915e8
BrandonWade <brandon_wade2005@hotmail.com>
2021-02-27 16:43:30 -0600

    Add Link component, improve CSS for Login page.

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,4 +1,6 @@
-import Input from '../../components/Input';
+import Logo from '../../components/Logo';
+import { InputField } from '../../components/Input';
+import Link from '../../components/Link';
 import Button from '../../components/Button';
 import './Login.scss';
 
@@ -6,11 +8,18 @@ function Login() {
     return (
         <div className='Login'>
             <div className='Login-content'>
-                Username:
-                <Input />
-                Password:
-                <Input />
-                <Button>Log In</Button>
+                <Logo className='Login-logo' size='large' />
+                <form className='Login-form'>
+                    <InputField label='Email' rowClassName='Login-inputRow' labelClassName='Login-inputLabel' className='Login-input' />
+                    <InputField label='Password' rowClassName='Login-inputRow' labelClassName='Login-inputLabel' className='Login-input' />
+                    <div className='Login-forgotPasswordContainer'>
+                        <Link>Forgot your password?</Link>
+                    </div>
+                    <Button className='Login-submit'>Login</Button>
+                    <div className='Login-registerContainer'>
+                        Don't have an account yet? <Link>Register!</Link>
+                    </div>
+                </form>
             </div>
         </div>
     );
