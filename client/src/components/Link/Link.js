1b5bb5898536dca15869ba386a19a583fe8427a0
BrandonWade <brandon_wade2005@hotmail.com>
2021-02-27 22:23:52 -0600

    Update Link to support internal and external links.

--- client/src/components/Link/Link.js
+++ client/src/components/Link/Link.js
@@ -1,11 +1,25 @@
+import { Link as InternalLink } from 'react-router-dom';
 import './Link.scss';
 
-function Link({ className = '', href = '', children = [] }) {
+function renderInternalLink({ className = '', to = '', children = [] }) {
     return (
-        <a className={`Link ${className}`} href={href}>
+        <InternalLink className={`Link ${className}`} to={to}>
+            {children}
+        </InternalLink>
+    );
+}
+
+function renderExternalLink({ className = '', to = '', children = [] }) {
+    return (
+        <a className={`Link ${className}`} href={to} target='_blank'>
             {children}
         </a>
     );
+}
+
+function Link(props) {
+    const isInternal = (props.to || '').indexOf('/') === 0;
+    return isInternal ? renderInternalLink(props) : renderExternalLink(props);
 }
 
 export default Link;
--- client/src/pages/About/About.js
+++ client/src/pages/About/About.js
@@ -10,15 +10,15 @@ function About() {
                     <h3 className='About-sectionHeading'>Wizard of the Coast Fan Content</h3>
                     <p className='About-sectionText'>
                         Blackblade is unofficial Fan Content permitted under the{' '}
-                        <Link href='https://company.wizards.com/fancontentpolicy'>Wizards of the Coast Fan Content Policy</Link>. Blackblade is in no
-                        way approved or endorsed by Wizards. Portions of the materials (including but not limited to oracle text, card images, and
-                        card logos) used are property of Wizards of the Coast. &copy; Wizards of the Coast LLC.
+                        <Link to='https://company.wizards.com/en/legal/fancontentpolicy'>Wizards of the Coast Fan Content Policy</Link>. Blackblade is
+                        in no way approved or endorsed by Wizards. Portions of the materials (including but not limited to oracle text, card images,
+                        and card logos) used are property of Wizards of the Coast. &copy; Wizards of the Coast LLC.
                     </p>
                 </section>
                 <section className='About-section'>
                     <h3 className='About-sectionHeading'>Scryfall</h3>
                     <p className='About-sectionText'>
-                        Blackblade uses <Link href='https://scryfall.com/'>Scryfall</Link> as the source of it's data and images. Blackblade is in no
+                        Blackblade uses <Link to='https://scryfall.com/'>Scryfall</Link> as the source of it's data and images. Blackblade is in no
                         way approved by, endorsed by, or related to Scryfall. Please go and support them and the work they do - this website cannot
                         exist without them.
                     </p>
@@ -26,17 +26,17 @@ function About() {
                 <section className='About-section'>
                     <h3 className='About-sectionHeading'>Prices</h3>
                     <p className='About-sectionText'>
-                        Card pricing information comes from <Link href='https://scryfall.com/'>Scryfall</Link>, which in turn comes from{' '}
-                        <Link href='https://www.tcgplayer.com/'>TCGPlayer</Link>. Prices are in USD, and are <strong>ONLY</strong> estimates. Prices
-                        are updated weekly, and are in no way a guarantee or a promise - they are only intended to be used as a loose guideline.
+                        Card pricing information comes from <Link to='https://scryfall.com/'>Scryfall</Link>, which in turn comes from{' '}
+                        <Link to='https://www.tcgplayer.com/'>TCGPlayer</Link>. Prices are in USD, and are <strong>ONLY</strong> estimates. Prices are
+                        updated weekly, and are in no way a guarantee or a promise - they are only intended to be used as a loose guideline.
                     </p>
                 </section>
                 <section className='About-section'>
                     <h3 className='About-sectionHeading'>Source Code</h3>
                     <p className='About-sectionText'>
                         Blackblade is open source. You can find the code for the website{' '}
-                        <Link href='https://github.com/BrandonWade/blackblade'>here</Link>, and the code for the batch that retrieves Scryfall's data
-                        weekly can be found <Link href='https://github.com/BrandonWade/blackblade-batch'>here</Link>.
+                        <Link to='https://github.com/BrandonWade/blackblade'>here</Link>, and the code for the batch that retrieves Scryfall's data
+                        weekly can be found <Link to='https://github.com/BrandonWade/blackblade-batch'>here</Link>.
                     </p>
                 </section>
             </div>
