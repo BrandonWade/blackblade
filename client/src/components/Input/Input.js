bbfa74ffcc47117a53aab0fb29196af7dbbb18bd
BrandonWade <brandon_wade2005@hotmail.com>
2021-02-28 00:06:50 -0600

    Add hook for login flow.

--- client/src/components/Input/Input.js
+++ client/src/components/Input/Input.js
@@ -1,8 +1,18 @@
 import withFormField from '../../hocs/withFormField';
 import './Input.scss';
 
-function Input({ type = 'text', className = '', placeholder = '', name = '', value = '', onChange = () => {} }) {
-    return <input type={type} className={`Input ${className}`} placeholder={placeholder} name={name} value={value} onChange={onChange} />;
+function Input({ type = 'text', className = '', placeholder = '', name = '', value = '', autoComplete = '', onChange = () => {} }) {
+    return (
+        <input
+            type={type}
+            className={`Input ${className}`}
+            placeholder={placeholder}
+            name={name}
+            value={value}
+            autoComplete={autoComplete}
+            onChange={onChange}
+        />
+    );
 }
 
 export default Input;
--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,3 +1,6 @@
+import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import useAuth from '../../hooks/useAuth';
 import Logo from '../../components/Logo';
 import { InputField } from '../../components/Input';
 import Link from '../../components/Link';
@@ -5,17 +8,63 @@ import Button from '../../components/Button';
 import './Login.scss';
 
 function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const history = useHistory();
+    const { login } = useAuth();
+
+    const onChangeEmail = e => {
+        setEmail(e.target.value);
+    };
+
+    const onChangePassword = e => {
+        setPassword(e.target.value);
+    };
+
+    const onSubmit = async e => {
+        e.preventDefault();
+
+        if (email === '' || password === '') {
+            return;
+        }
+
+        const result = await login(email, password);
+        if (result?.success) {
+            // TODO: Set authenticated prop
+            history.push('/');
+        }
+    };
+
     return (
         <div className='Login'>
             <div className='Login-content'>
                 <Logo className='Login-logo' size='large' />
                 <form className='Login-form'>
-                    <InputField label='Email' rowClassName='Login-inputRow' labelClassName='Login-inputLabel' className='Login-input' />
-                    <InputField label='Password' rowClassName='Login-inputRow' labelClassName='Login-inputLabel' className='Login-input' />
+                    <InputField
+                        label='Email'
+                        rowClassName='Login-inputRow'
+                        labelClassName='Login-inputLabel'
+                        className='Login-input'
+                        autoComplete='username'
+                        value={email}
+                        onChange={onChangeEmail}
+                    />
+                    <InputField
+                        type='password'
+                        label='Password'
+                        rowClassName='Login-inputRow'
+                        labelClassName='Login-inputLabel'
+                        className='Login-input'
+                        autoComplete='current-password'
+                        value={password}
+                        onChange={onChangePassword}
+                    />
                     <div className='Login-forgotPasswordContainer'>
                         <Link>Forgot your password?</Link>
                     </div>
-                    <Button className='Login-submit'>Login</Button>
+                    <Button className='Login-submit' onClick={onSubmit}>
+                        Login
+                    </Button>
                     <div className='Login-registerContainer'>
                         Don't have an account yet? <Link>Register!</Link>
                     </div>
