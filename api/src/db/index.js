a88075dce2e275568a85895a3af1b23d59c27152
BrandonWade <brandon_wade2005@hotmail.com>
2021-03-02 21:13:49 -0600

    Begin setting up session logic.

--- api/src/db/index.js
+++ api/src/db/index.js
@@ -1,17 +1,18 @@
 import mysql from 'mysql2/promise';
 import Knex from 'knex';
 
-const { DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE } = process.env;
-const credentials = {
+const { DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_PORT } = process.env;
+const config = {
     host: DB_HOST,
     user: DB_USERNAME,
     password: DB_PASSWORD,
     database: DB_DATABASE,
+    port: DB_PORT,
 };
 
-export const connection = mysql.createPool(credentials);
+export const connection = mysql.createPool(config);
 
 export const builder = Knex({
     client: 'mysql2',
-    connection: credentials,
+    connection: config,
 });
