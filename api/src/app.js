08eb0aa49b606357ad26d520017154944fcfea5a
BrandonWade <brandon_wade2005@hotmail.com>
2021-03-02 21:13:49 -0600

    Begin setting up session logic.

--- api/src/app.js
+++ api/src/app.js
@@ -1,14 +1,36 @@
 import express from 'express';
-import cookieParser from 'cookie-parser';
 import logger from 'morgan';
+import cookieParser from 'cookie-parser';
+import session from 'express-session';
+import MySQLStore from 'express-mysql-session';
+import { connection } from './db';
 import router from './routes';
 
 const app = express();
+
+const sessionSecret = process.env.SESSION_SECRET || '';
+if (!sessionSecret) {
+    throw 'session secret not loaded';
+}
+
+const sessionStore = MySQLStore(session);
+const sessionMiddleware = new session({
+    key: 'bb_session',
+    secret: sessionSecret,
+    store: new sessionStore({}, connection),
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        httpOnly: true,
+        secure: true,
+    },
+});
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(sessionMiddleware);
 
 app.use('/', router);
 
