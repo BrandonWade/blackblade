b3885b61bbee2668eb4308f839dcb67ffa5a3d59
BrandonWade <brandon_wade2005@hotmail.com>
2021-03-25 01:12:56 -0500

    Only enable certain deck controls when logged in.

--- client/src/components/DeckActions/DeckActions.js
+++ client/src/components/DeckActions/DeckActions.js
@@ -1,14 +1,20 @@
+import { useContext } from 'react';
 import { useParams } from 'react-router-dom';
+import AuthContext from '../../contexts/Auth';
+import DeckBuilderContext from '../../contexts/DeckBuilder';
 import DeckActionButton from './DeckActionButton';
 import { Pencil } from '../Icons';
 import './DeckActions.scss';
 
 function DeckActions() {
     const { publicID } = useParams();
+    const { accountPublicID } = useContext(AuthContext);
+    const { deckAccountPublicID } = useContext(DeckBuilderContext);
+    const ownsDeck = accountPublicID === deckAccountPublicID;
 
     return (
         <div className='DeckActions'>
-            <DeckActionButton to={`/decks/${publicID}/edit`}>
+            <DeckActionButton to={`/decks/${publicID}/edit`} visible={ownsDeck}>
                 <Pencil />
                 <span className='DeckActions-buttonLabel'>Edit Deck</span>
             </DeckActionButton>
