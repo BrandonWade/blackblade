d9726fd85b8f6ed33db1935a8ff75fb9523bd048
BrandonWade <brandon_wade2005@hotmail.com>
2021-04-06 14:19:42 -0500

    Fix password regex.

--- api/src/validators/auth.js
+++ api/src/validators/auth.js
@@ -4,7 +4,7 @@ const emailValid = body('email').exists().isEmail();
 const passwordValid = body('password')
     .exists()
     .isLength({ min: 15, max: 50 })
-    .matches(/^[\w\!\@\#\$\%\^\*]+$/);
+    .matches(/^[\w!@#$%^&*]+$/);
 
 const loginValidators = [emailValid, passwordValid];
 
