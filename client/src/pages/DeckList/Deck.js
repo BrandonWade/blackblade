bb89f17a7a3c4d4c8ca762ef59ff89d81ca8b522
BrandonWade <brandon_wade2005@hotmail.com>
2021-04-10 22:04:54 -0500

    Update deck list css.

--- client/src/pages/DeckList/Deck.js
+++ client/src/pages/DeckList/Deck.js
@@ -1,30 +1,22 @@
-function Deck({ publicID = '', name = '', size = 0, colors = {}, removeDeck = () => {} }) {
+import { Link } from 'react-router-dom';
+import useSymbols from '../../hooks/useSymbols';
+
+function Deck({ publicID = '', name = '', size = 0, colors = '', removeDeck = () => {} }) {
     const onRemoveClick = () => {
         removeDeck(publicID);
     };
 
-    const renderColors = () => {
-        return (
-            <>
-                <img className='DeckList-color' src='https://c2.scryfall.com/file/scryfall-symbols/card-symbols/G.svg' alt='Symbol G' />
-                <img className='DeckList-color' src='https://c2.scryfall.com/file/scryfall-symbols/card-symbols/U.svg' alt='Symbol U' />
-                <img className='DeckList-color' src='https://c2.scryfall.com/file/scryfall-symbols/card-symbols/R.svg' alt='Symbol R' />
-                <img className='DeckList-color' src='https://c2.scryfall.com/file/scryfall-symbols/card-symbols/B.svg' alt='Symbol B' />
-                <img className='DeckList-color' src='https://c2.scryfall.com/file/scryfall-symbols/card-symbols/W.svg' alt='Symbol W' />
-                <img className='DeckList-color' src='https://c2.scryfall.com/file/scryfall-symbols/card-symbols/C.svg' alt='Symbol C' />
-            </>
-        );
-    };
-
     return (
-        <div key={publicID} className='DeckList-deck'>
-            <div className='DeckList-delete' onClick={onRemoveClick}>
-                &#x2715;
+        <Link to={`/decks/${publicID}`}>
+            <div key={publicID} className='DeckList-deck'>
+                <div className='DeckList-delete' onClick={onRemoveClick}>
+                    &#x2715;
+                </div>
+                <div className='DeckList-deckName'>{name}</div>
+                <div className='DeckList-deckSize'>{`${size} cards`}</div>
+                <div className='DeckList-deckColors' dangerouslySetInnerHTML={{ __html: useSymbols(colors) }} />
             </div>
-            <div className='DeckList-deckName'>{name}</div>
-            <div className='DeckList-deckSize'>{`${size} cards`}</div>
-            <div className='DeckList-deckColors'>{renderColors()}</div>
-        </div>
+        </Link>
     );
 }
 
