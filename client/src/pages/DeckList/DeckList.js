f6e73f07a2d9f676108b9b89155f35f3dc6bf454
BrandonWade <brandon_wade2005@hotmail.com>
2021-04-13 00:18:26 -0500

    Connect list decks api to frontend.

--- client/src/pages/DeckList/DeckList.js
+++ client/src/pages/DeckList/DeckList.js
@@ -1,11 +1,27 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import useDeck from '../../hooks/useDeck';
 import DeckListContext from '../../contexts/DeckList';
 import HeaderPage from '../../components/HeaderPage';
 import Deck from './Deck';
 import './DeckList.scss';
 
 function DeckList() {
+    const { listDecks } = useDeck();
     const { deckList, setDeckList } = useContext(DeckListContext);
+
+    useEffect(() => {
+        const fetchDeckList = async () => {
+            const result = await listDecks();
+            if (!result.success) {
+                return;
+            }
+
+            setDeckList(result.decks);
+        };
+
+        fetchDeckList();
+    }, []);
 
     const removeDeck = publicID => {
         setDeckList(deckList.filter(d => d.public_id !== publicID));
@@ -15,6 +31,11 @@ function DeckList() {
         <HeaderPage className='DeckList'>
             <div className='DeckList-content'>
                 <div className='DeckList-list'>
+                    <Link to='/decks/new' className='DeckList-deck'>
+                        <div className='DeckList-newDeck'>
+                            <div>New Deck</div>
+                        </div>
+                    </Link>
                     {deckList.map(deck => (
                         <Deck publicID={deck.public_id} name={deck.name} size={deck.size} colors={deck.colors} removeDeck={removeDeck} />
                     ))}
