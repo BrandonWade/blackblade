2aad910f7717f7bdf04735106a094e004cd2c7b0
BrandonWade <brandon_wade2005@hotmail.com>
2021-04-16 20:04:33 -0500

    Display confirm dialog when deleting a deck.

--- client/src/components/Backdrop/Backdrop.js
+++ client/src/components/Backdrop/Backdrop.js
@@ -1,12 +1,12 @@
 import './Backdrop.scss';
 
 function Backdrop({ className = '', visible = false, children = [] }) {
-    return (
-        <div className={`Backdrop ${className} ${visible ? '' : 'Backdrop--closed'}`}>
+    return visible ? (
+        <div className={`Backdrop ${className}`}>
             <div className='Backdrop-shroud' />
             {children}
         </div>
-    );
+    ) : null;
 }
 
 export default Backdrop;
