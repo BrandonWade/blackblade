cefea3a3cdeba377ad1118322c64ee6496e91e18
BrandonWade <brandon_wade2005@hotmail.com>
2021-05-02 02:20:34 -0500

    Add support for closing Backdrop when pressing Escape.

--- client/src/components/Backdrop/Backdrop.js
+++ client/src/components/Backdrop/Backdrop.js
@@ -1,6 +1,26 @@
+import { useEffect } from 'react';
 import './Backdrop.scss';
 
-function Backdrop({ className = '', visible = false, children = [], onClick = () => {} }) {
+function Backdrop({ className = '', visible = false, children = [], onClose = () => {}, closeOnClick = true, closeOnEscape = true }) {
+    useEffect(() => {
+        if (closeOnEscape) {
+            const onEscapePress = e => {
+                if (e.keyCode === 27) {
+                    onClose();
+                }
+            };
+
+            document.addEventListener('keydown', onEscapePress, false);
+            return () => document.removeEventListener('keydown', onEscapePress, false);
+        }
+    }, []);
+
+    const onClick = () => {
+        if (closeOnClick) {
+            onClose();
+        }
+    };
+
     const stopClickPropagation = e => e.stopPropagation();
 
     return visible ? (
