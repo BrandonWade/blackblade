e739be9a3c3f20845821f51a7c72b51ac03c57db
BrandonWade <brandon_wade2005@hotmail.com>
2021-06-14 00:07:02 -0500

    Add link to infra repo to About page.

--- client/src/pages/About/About.js
+++ client/src/pages/About/About.js
@@ -35,8 +35,9 @@ function About() {
                     <h3 className='About-sectionHeading'>Source Code</h3>
                     <p className='About-sectionText'>
                         Blackblade is open source. You can find the code for the website{' '}
-                        <Link to='https://github.com/BrandonWade/blackblade'>here</Link>, and the code for the batch that retrieves Scryfall's data
-                        weekly can be found <Link to='https://github.com/BrandonWade/blackblade-batch'>here</Link>.
+                        <Link to='https://github.com/BrandonWade/blackblade'>here</Link>. The code for the infrastructure can be found{' '}
+                        <Link to='https://github.com/BrandonWade/blackblade-infrastructure'>here</Link>, and the code for the batch that retrieves
+                        Scryfall's data weekly can be found <Link to='https://github.com/BrandonWade/blackblade-batch'>here</Link>.
                     </p>
                 </section>
             </div>
