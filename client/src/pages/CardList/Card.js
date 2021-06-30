e3aa4c363fe545ab56788ddfe1f1051f5f53d528
BrandonWade <brandon_wade2005@hotmail.com>
2021-06-30 18:08:23 -0500

    Update CloseButton CSS.

--- client/src/pages/CardList/Card.js
+++ client/src/pages/CardList/Card.js
@@ -1,12 +1,17 @@
+import CloseButton from '../../components/CloseButton';
+
 function Card({ cardID = 0, image = '', name = '', tags = [] }) {
     return (
-        <a className='CardList-card' href={`/cards/${cardID}`}>
-            <img className='CardList-cardImage' src={image} alt={name} />
+        <div className='CardList-cardBlock'>
+            <a className='CardList-cardLink' href={`/cards/${cardID}`}>
+                <img className='CardList-cardImage' src={image} alt={name} />
+            </a>
             <div className='CardList-cardName'>{name}</div>
             {tags.map(tag => (
                 <div key={tag.id}>{tag.text}</div>
             ))}
-        </a>
+            <CloseButton className='CardList-removeCardButton' />
+        </div>
     );
 }
 
