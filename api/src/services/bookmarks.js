7889649b1e0c4db89f4c127349ea079b553a2ce3
BrandonWade <brandon_wade2005@hotmail.com>
2021-07-11 00:03:03 -0500

    Return bookmark on create.

--- api/src/services/bookmarks.js
+++ api/src/services/bookmarks.js
@@ -1,14 +1,46 @@
 import BookmarkRepository from '../repositories/bookmarks';
 
 const createBookmark = async (cardID, accountID) => {
+    let bookmark;
+
     try {
-        await BookmarkRepository.createBookmark(cardID, accountID);
+        // Check if the bookmark already exists
+        const [existsResult] = await BookmarkRepository.getBookmarkByCardID(
+            cardID,
+            accountID,
+        );
+        if (existsResult?.length === 1 && existsResult?.[0]?.id) {
+            return {
+                created: false,
+                bookmark: existsResult?.[0],
+            };
+        }
+
+        // If it doesn't, create a new one and return it
+        const [createResult] = await BookmarkRepository.createBookmark(
+            cardID,
+            accountID,
+        );
+        const bookmarkID = createResult?.insertId || 0;
+        if (!bookmarkID) {
+            throw 'error inserting new bookmark row';
+        }
+
+        const [getResult] = await BookmarkRepository.getBookmark(bookmarkID);
+        if (getResult?.length !== 1 || !getResult?.[0]?.id) {
+            throw `error getting created bookmark with id ${bookmarkID}`;
+        }
+
+        bookmark = getResult[0];
     } catch (e) {
         console.error('error creating bookmark', e);
         throw e;
     }
 
-    return;
+    return {
+        created: true,
+        bookmark,
+    };
 };
 
 const listBookmarks = async (accountID) => {
