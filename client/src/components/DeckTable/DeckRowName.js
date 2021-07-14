8c48b4507b79475f4a9748b343ab91ff203d4427
BrandonWade <brandon_wade2005@hotmail.com>
2021-07-14 18:51:13 -0500

    Update preview position based on hovered card link.

--- client/src/components/DeckTable/DeckRowName.js
+++ client/src/components/DeckTable/DeckRowName.js
@@ -1,10 +1,10 @@
 import { Link } from 'react-router-dom';
 import withImagePreview from '../../hocs/withImagePreview';
 
-function DeckRowName({ cardID = 0, name = '', onMouseEnter = () => {}, onMouseLeave = () => {} }) {
+function DeckRowName({ innerRef = null, cardID = 0, name = '', onMouseEnter = () => {}, onMouseLeave = () => {} }) {
     return (
         <Link to={`/cards/${cardID}`}>
-            <div className='DeckTable-cardLink' onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
+            <div ref={innerRef} className='DeckTable-cardLink' onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
                 {name}
             </div>
         </Link>
