0ce935758dc00662e24eb643519f7c1371d0f58f
BrandonWade <brandon_wade2005@hotmail.com>
2021-07-14 18:51:13 -0500

    Update preview position based on hovered card link.

--- client/src/hocs/withImagePreview/withImagePreview.js
+++ client/src/hocs/withImagePreview/withImagePreview.js
@@ -4,24 +4,40 @@ import './withImagePreview.scss';
 function withImagePreview(BaseComponent) {
     const ImagePreview = ({ children = [], image = '', alt = '', ...rest }) => {
         const [visible, setVisible] = useState(false);
+        const [styles, setStyles] = useState({});
         const ref = useRef({ x: null, y: null });
 
-        const onMouseEnter = e => {
+        const onMouseEnter = () => {
+            const { x, y } = ref.current.getBoundingClientRect();
+
+            // TODO: Calculate these using card dimentions
+            const offsetX = 235; // card width plus padding (TODO: Calculate this)
+            const offsetY = 125; // approximately half of card height
+
+            let top = y - offsetY;
+            let left = x - offsetX;
+
+            // TODO: Bound top and left with viewport
+
+            setStyles({
+                top,
+                left,
+            });
             setVisible(true);
         };
 
-        const onMouseLeave = e => {
+        const onMouseLeave = () => {
             setVisible(false);
         };
 
         return (
             <>
                 {visible ? (
-                    <div className='ImagePreview'>
+                    <div className='ImagePreview' style={styles}>
                         <img className='ImagePreview-image' src={image} alt={alt} />
                     </div>
                 ) : null}
-                <BaseComponent {...rest} ref={ref} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
+                <BaseComponent {...rest} innerRef={ref} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
                     {children}
                 </BaseComponent>
             </>
