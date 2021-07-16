ace8ea6c393d48d264328c6a48c72eb195252eca
BrandonWade <brandon_wade2005@hotmail.com>
2021-07-15 19:06:42 -0500

    Bound the preview to the viewport parameters if possible.

--- client/src/hocs/withSetCardImagePreviewPosition/withSetCardImagePreviewPosition.js
+++ client/src/hocs/withSetCardImagePreviewPosition/withSetCardImagePreviewPosition.js
@@ -8,17 +8,40 @@ function withSetCardImagePreviewPosition(BaseComponent) {
         const { setTop, setLeft, setImage, setVisible } = useContext(CardImagePreviewContext);
         const imageWidth = useCSSVariableValue('--card-image-preview-image-width');
         const imageHeight = useCSSVariableValue('--card-image-preview-image-height');
+        const halfImageHeight = imageHeight / 2;
 
         const onMouseEnter = () => {
-            const { x, y, height } = ref.current.getBoundingClientRect();
+            const { x, y, height: componentHeight } = ref.current.getBoundingClientRect();
+            const offsetX = imageWidth + 5; // include a small gap between preview and component
+            const offsetY = halfImageHeight - componentHeight / 2; // to center vertically
 
-            const offsetX = imageWidth + 5; // add a small gap between preview and component
-            const offsetY = imageHeight / 2 - height / 2; // to center vertically
-
+            // Calculate the "normal" top and left
             let top = y - offsetY;
             let left = x - offsetX;
 
-            // TODO: Bound top and left with viewport
+            // Bound the top and left if the preview doesn't fit in the viewport
+            const viewportWidth = window.innerWidth;
+            const viewportHeight = window.innerHeight;
+            const bottom = top + imageHeight;
+            const right = left + imageWidth;
+
+            // Bound the image vertically
+            if (imageHeight < viewportHeight) {
+                if (top < 0) {
+                    top = 0;
+                } else if (bottom > viewportHeight) {
+                    top = viewportHeight - imageHeight;
+                }
+            }
+
+            // Bound the image horizontally
+            if (imageWidth < viewportWidth) {
+                if (left < 0) {
+                    left = 0;
+                } else if (right > viewportWidth) {
+                    left = viewportWidth - imageWidth;
+                }
+            }
 
             setTop(top);
             setLeft(left);
