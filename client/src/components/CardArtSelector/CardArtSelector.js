ab13a15eacc2e4a10e7379115a35c40bbb711e98
BrandonWade <brandon_wade2005@hotmail.com>
2021-08-03 01:46:56 -0500

    Fix react error when changing card art.

--- client/src/components/CardArtSelector/CardArtSelector.js
+++ client/src/components/CardArtSelector/CardArtSelector.js
@@ -16,16 +16,42 @@ function CardArtSelector() {
 
     const onClose = () => setArtSelectorVisible(false);
 
+    // TODO: Move most of this handling into reducer
     const onSelectCard = cardVariant => {
+        // If the chosen variant is already selected, do nothing
+        if (cardVariant.card_id === card.card_id) {
+            setArtSelectorVisible(false);
+            return;
+        }
+
         const cardList = inDeck ? deckCards : maybeboardCards;
         const updateCardList = inDeck ? setDeckCards : setMaybeboardCards;
-        const index = cardList.findIndex(c => c.card_id === card.card_id);
-        const updatedCard = {
-            ...cardList[index],
-            ...cardVariant,
-            selection_type: 'manual',
-        };
-        const cards = [...cardList.slice(0, index), updatedCard, ...cardList.slice(index + 1)];
+        const cardIndex = cardList.findIndex(c => c.card_id === card.card_id);
+        const variantIndex = cardList.findIndex(c => c.card_id === cardVariant.card_id);
+        let cards;
+        let updatedCard;
+
+        // Check if the selected variant already exists in the list
+        if (variantIndex === -1) {
+            updatedCard = {
+                ...cardList[cardIndex],
+                ...cardVariant,
+                selection_type: 'manual',
+            };
+            cards = [...cardList.slice(0, cardIndex), updatedCard, ...cardList.slice(cardIndex + 1)];
+        } else {
+            // If the variant does exist, remove the previous card and combine the counts
+            const count = (parseInt(cardList[cardIndex].count) || 1) + (parseInt(cardList[variantIndex].count) || 1);
+            updatedCard = {
+                ...cardList[cardIndex],
+                ...cardVariant,
+                selection_type: 'manual',
+                count,
+            };
+            cards = [...cardList.slice(0, variantIndex), updatedCard, ...cardList.slice(variantIndex + 1)];
+            cards = cards.filter(c => c.card_id !== card.card_id);
+        }
+
         setCard(updatedCard);
         updateCardList(cards);
         setArtSelectorVisible(false);
