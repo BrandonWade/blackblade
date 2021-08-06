3cd865dbcce266793074d3b5dbedc3bb3657a075
BrandonWade <brandon_wade2005@hotmail.com>
2021-08-05 21:40:50 -0500

    FIx bug where public decks could not be exported.

--- client/src/components/DeckActions/DeckActions.js
+++ client/src/components/DeckActions/DeckActions.js
@@ -3,7 +3,7 @@ import { useParams, useHistory } from 'react-router-dom';
 import useDecks from '../../hooks/useDecks';
 import useMessage from '../../hooks/useMessage';
 import AuthContext from '../../contexts/Auth';
-import DeckBuilderContext from '../../contexts/DeckBuilder';
+import DeckBuilderContext, { isDeckUnmodified } from '../../contexts/DeckBuilder';
 import ExportDeckDialogContext from '../../contexts/ExportDeckDialog';
 import DeckActionButton from './DeckActionButton';
 import { Pencil, Export } from '../Icons';
@@ -21,6 +21,10 @@ function DeckActions() {
         deckVisibility,
         deckCards,
         maybeboardCards,
+        unmodifiedDeckName,
+        unmodifiedDeckVisibility,
+        unmodifiedDeckCards,
+        unmodifiedMaybeboardCards,
         setUnmodifiedDeckName,
         setUnmodifiedDeckVisibility,
         setUnmodifiedDeckCards,
@@ -29,18 +33,30 @@ function DeckActions() {
     const { setDeckExport, setVisible } = useContext(ExportDeckDialogContext);
     const ownsDeck = accountPublicID === deckAccountPublicID;
     const hasCards = deckCards.length > 0 || maybeboardCards.length > 0;
+    const isUnmodified = isDeckUnmodified(
+        deckName,
+        deckVisibility,
+        deckCards,
+        maybeboardCards,
+        unmodifiedDeckName,
+        unmodifiedDeckVisibility,
+        unmodifiedDeckCards,
+        unmodifiedMaybeboardCards
+    );
 
     const onEditDeck = () => {
         history.replace(`/decks/${publicID}/edit`);
     };
 
     const onExportDeck = async () => {
-        // Since the deck export comes from the backend, ensure we persist the latest deck state before exporting
-        const saveResult = await saveDeck(publicID, deckName, deckVisibility, deckCards, maybeboardCards);
-        if (!saveResult?.success) {
-            const { text, type } = saveResult?.message;
-            showMessage(text, type);
-            return;
+        if (!isUnmodified) {
+            // Since the deck export comes from the backend, ensure we persist the latest deck state before exporting
+            const saveResult = await saveDeck(publicID, deckName, deckVisibility, deckCards, maybeboardCards);
+            if (!saveResult?.success) {
+                const { text, type } = saveResult?.message;
+                showMessage(text, type);
+                return;
+            }
         }
 
         // Once changes to the deck have been saved, update the unmodified state
