2cbcfbcfd1d0cdf83f73a98941e42db105ab510f
BrandonWade <brandon_wade2005@hotmail.com>
2021-08-07 16:27:24 -0500

    Remove useRandomCard hook and move logic into page.

--- client/src/pages/Random/Random.js
+++ client/src/pages/Random/Random.js
@@ -1,11 +1,24 @@
-import { useEffect } from 'react';
-import useRandomCard from '../../hooks/useRandomCard';
+import { useContext, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import useSearch from '../../hooks/useSearch';
+import CardContext from '../../contexts/Card';
 import HeaderPage from '../../components/HeaderPage';
 
 function Random() {
-    const { displayRandomCard } = useRandomCard();
+    const history = useHistory();
+    const { getRandomCard } = useSearch();
+    const { setCard } = useContext(CardContext);
 
     useEffect(() => {
+        const displayRandomCard = async () => {
+            const response = await getRandomCard();
+            if (!response.success) {
+                return;
+            }
+
+            setCard(response.results);
+            history.replace(response.redirect);
+        };
         displayRandomCard();
     }, []);
 
