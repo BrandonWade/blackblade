9c73a731a935be747bba8ef0b2c314a9182723fb
BrandonWade <brandon_wade2005@hotmail.com>
2021-09-25 12:01:06 -0500

    Add more search controller test cases.

--- api/src/controllers/search.test.js
+++ api/src/controllers/search.test.js
@@ -12,7 +12,7 @@ import {
 
 jest.mock('../services/search');
 
-describe.only('Search Controller', () => {
+describe('Search Controller', () => {
     describe('searchCards', () => {
         test('returns an error if one occurred while searching for cards', async () => {
             const query = {
@@ -137,6 +137,137 @@ describe.only('Search Controller', () => {
             SearchService.getCardByID.mockResolvedValue(result);
 
             await getCardByID(req, res);
+
+            expect(res.status).toHaveBeenCalledWith(StatusCodes.OK);
+            expect(res.json).toHaveBeenCalledWith(result);
+        });
+    });
+
+    describe('getRandomCard', () => {
+        test('returns an error if one occurred while retrieving a random card', async () => {
+            const req = requestMock({});
+            const res = responseMock();
+            SearchService.getRandomCard.mockImplementation(() => {
+                throw new Error();
+            });
+
+            await getRandomCard(req, res);
+
+            expect(res.status).toHaveBeenCalledWith(
+                StatusCodes.INTERNAL_SERVER_ERROR,
+            );
+            expect(res.json).toHaveBeenCalledWith({
+                message: {
+                    type: 'error',
+                    text: 'An error occurred while retrieving a random card.',
+                },
+            });
+        });
+
+        test('returns a random card', async () => {
+            const req = requestMock({});
+            const res = responseMock();
+            const result = {
+                id: 12345,
+                layout: 'private',
+                name: 'test name',
+                set_name: 'test set',
+                set_code: 'test',
+                faces_json: {},
+                sets_json: {},
+                rulings_json: {},
+            };
+            SearchService.getRandomCard.mockResolvedValue(result);
+
+            await getRandomCard(req, res);
+
+            expect(res.status).toHaveBeenCalledWith(StatusCodes.OK);
+            expect(res.json).toHaveBeenCalledWith(result);
+        });
+    });
+
+    describe('getCardTypes', () => {
+        test('returns an error if one occurred while retrieving the list of types', async () => {
+            const req = requestMock({});
+            const res = responseMock();
+            SearchService.getCardTypes.mockImplementation(() => {
+                throw new Error();
+            });
+
+            await getCardTypes(req, res);
+
+            expect(res.status).toHaveBeenCalledWith(
+                StatusCodes.INTERNAL_SERVER_ERROR,
+            );
+            expect(res.json).toHaveBeenCalledWith({
+                message: {
+                    type: 'error',
+                    text: 'An error occurred while retrieving available card types.',
+                },
+            });
+        });
+
+        test('returns the list of types', async () => {
+            const req = requestMock({});
+            const res = responseMock();
+            const result = [
+                {
+                    id: 123,
+                    type: 'test type 1',
+                },
+                {
+                    id: 456,
+                    type: 'test type 2',
+                },
+            ];
+            SearchService.getCardTypes.mockResolvedValue(result);
+
+            await getCardTypes(req, res);
+
+            expect(res.status).toHaveBeenCalledWith(StatusCodes.OK);
+            expect(res.json).toHaveBeenCalledWith(result);
+        });
+    });
+
+    describe('getCardSets', () => {
+        test('returns an error if one occurred while retrieving the list of sets', async () => {
+            const req = requestMock({});
+            const res = responseMock();
+            SearchService.getCardSets.mockImplementation(() => {
+                throw new Error();
+            });
+
+            await getCardSets(req, res);
+
+            expect(res.status).toHaveBeenCalledWith(
+                StatusCodes.INTERNAL_SERVER_ERROR,
+            );
+            expect(res.json).toHaveBeenCalledWith({
+                message: {
+                    type: 'error',
+                    text: 'An error occurred while retrieving available card sets.',
+                },
+            });
+        });
+
+        test('returns the list of sets', async () => {
+            const req = requestMock({});
+            const res = responseMock();
+            const result = [
+                {
+                    id: 123,
+                    set_code: 'test1',
+                    set_name: 'test set 1',
+                },
+                {
+                    id: 456,
+                    set_code: 'test2',
+                    set_name: 'test set 2',
+                },
+            ];
+            SearchService.getCardSets.mockResolvedValue(result);
+
+            await getCardSets(req, res);
 
             expect(res.status).toHaveBeenCalledWith(StatusCodes.OK);
             expect(res.json).toHaveBeenCalledWith(result);
