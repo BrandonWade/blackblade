b489b74cefee7e66732df89f4ff1e05b8c276373
BrandonWade <brandon_wade2005@hotmail.com>
2021-11-04 00:26:37 -0500

    Show separate card groups in DeckPreview.

--- client/src/components/DeckPreview/DeckPreview.js
+++ client/src/components/DeckPreview/DeckPreview.js
@@ -1,4 +1,5 @@
 import { useContext } from 'react';
+import useCardGroups from '../../hooks/useCardGroups';
 import DeckBuilderContext from '../../contexts/DeckBuilder';
 import BackgroundMessage, { EMPTY_DECK_PREVIEW } from '../BackgroundMessage';
 import CardGrid from '../CardGrid';
@@ -6,11 +7,14 @@ import './DeckPreview.scss';
 
 export default function DeckPreview() {
     const { deckCards, maybeboardCards } = useContext(DeckBuilderContext);
+    const { creatures, spells, land } = useCardGroups(deckCards);
 
     return (
         <div className='DeckPreview'>
             <BackgroundMessage showMessage={deckCards.length + maybeboardCards.length === 0} type={EMPTY_DECK_PREVIEW}>
-                {deckCards.length ? <CardGrid className='DeckPreview-cardGrid' cards={deckCards} isLink={true} /> : null}
+                {creatures.length ? <CardGrid className='DeckPreview-cardGrid' cards={creatures} isLink={true} /> : null}
+                {spells.length ? <CardGrid className='DeckPreview-cardGrid' cards={spells} isLink={true} /> : null}
+                {land.length ? <CardGrid className='DeckPreview-cardGrid' cards={land} isLink={true} /> : null}
                 {maybeboardCards.length ? <CardGrid className='DeckPreview-cardGrid' cards={maybeboardCards} isLink={true} /> : null}
             </BackgroundMessage>
         </div>
