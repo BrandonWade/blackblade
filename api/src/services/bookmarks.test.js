ba6ed5ea82300b11808b45980750efd365159ee1
BrandonWade <brandon_wade2005@hotmail.com>
2021-11-12 00:30:56 -0600

    Add more BookmarkService tests.

--- api/src/services/bookmarks.test.js
+++ api/src/services/bookmarks.test.js
@@ -16,5 +16,64 @@ describe('Bookmark Service', () => {
                 BookmarkService.listBookmarks(accountID),
             ).rejects.toThrow();
         });
+
+        test('returns the list of bookmarks for the provided account ID', async () => {
+            const accountID = 1;
+            const bookmarks = [
+                {
+                    id: 123,
+                    card_id: 456,
+                    name: 'test card',
+                    set_name: 'test set',
+                    set_code: 'test set code',
+                    layout: 'normal',
+                    faces_json: '{}',
+                },
+                {
+                    id: 321,
+                    card_id: 789,
+                    name: 'test card 2',
+                    set_name: 'test set 2',
+                    set_code: 'test set code 2',
+                    layout: 'normal',
+                    faces_json: '{}',
+                },
+            ];
+
+            BookmarkRepository.listBookmarks.mockResolvedValue([bookmarks]);
+            const output = await BookmarkService.listBookmarks(accountID);
+
+            expect(output).toEqual({
+                bookmarks,
+            });
+        });
+    });
+
+    describe('deleteBookmark', () => {
+        test('throws an error if one occurred while deleting the bookmark', async () => {
+            const bookmarkID = 123;
+            const accountID = 1;
+
+            BookmarkRepository.deleteBookmark.mockImplementation(() => {
+                throw new Error();
+            });
+
+            await expect(() =>
+                BookmarkService.deleteBookmark(bookmarkID, accountID),
+            ).rejects.toThrow();
+        });
+
+        test('returns nothing if the bookmark was successfully deleted', async () => {
+            const bookmarkID = 123;
+            const accountID = 1;
+
+            BookmarkRepository.deleteBookmark.mockResolvedValue();
+            const output = await BookmarkService.deleteBookmark(
+                bookmarkID,
+                accountID,
+            );
+
+            expect(output).toEqual();
+        });
     });
 });
