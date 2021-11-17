fd2eee225efd2f679002451dd04e84d2ec71985f
BrandonWade <brandon_wade2005@hotmail.com>
2021-11-17 17:09:37 -0600

    Add more tests for account service.

--- api/src/services/accounts.test.js
+++ api/src/services/accounts.test.js
@@ -3,8 +3,10 @@ import AccountService from './accounts';
 import EmailService from './email';
 import { hashValue, compareValues } from '../helpers/hash';
 import generateToken from '../helpers/tokens';
+import AlreadyExistsError from '../errors/already_exists';
 
 jest.mock('../repositories/accounts');
+jest.mock('../services/email');
 jest.mock('../helpers/hash');
 jest.mock('../helpers/tokens');
 
@@ -27,6 +29,48 @@ describe('Account Service', () => {
             );
 
             expect(output).toBe(true);
+        });
+
+        test('returns true if the account has already been registered but not activated', async () => {
+            const email = 'test@test.com';
+            const password = 'testpassword123';
+            const account = {
+                is_activated: false,
+            };
+
+            AccountRepository.getAccountByEmail.mockResolvedValue(account);
+            hashValue.mockResolvedValue('testhashvalue');
+            generateToken.mockResolvedValue('testtokenvalue');
+            AccountRepository.registerAccount.mockImplementation(() => {
+                throw new AlreadyExistsError();
+            });
+            EmailService.sendAccountActivationEmail.mockResolvedValue();
+
+            const output = await AccountService.registerAccount(
+                email,
+                password,
+            );
+
+            expect(output).toBe(true);
+        });
+
+        test('throws an error if one occurred while registering a new account', async () => {
+            const email = 'test@test.com';
+            const password = 'testpassword123';
+            const account = {
+                is_activated: false,
+            };
+
+            AccountRepository.getAccountByEmail.mockResolvedValue(account);
+            hashValue.mockResolvedValue('testhashvalue');
+            generateToken.mockResolvedValue('testtokenvalue');
+            AccountRepository.registerAccount.mockImplementation(() => {
+                throw new Error();
+            });
+
+            await expect(() =>
+                AccountService.registerAccount(email, password),
+            ).rejects.toThrow();
         });
     });
 });
