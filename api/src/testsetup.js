81a76a491ace505e58d5158a473f635f29f8f8e6
BrandonWade <brandon_wade2005@hotmail.com>
2022-01-17 00:35:51 -0600

    Disable console.error in unit tests.

--- api/src/testsetup.js
+++ api/src/testsetup.js
@@ -1,3 +1,11 @@
 process.env.ENVIRONMENT = 'test';
 process.env.MAILJET_PUBLIC_KEY = 'test_mj_public_key';
 process.env.MAILJET_PRIVATE_KEY = 'test_mj_private_key';
+
+global.console = {
+    error: jest.fn(),
+    log: console.log,
+    warn: console.warn,
+    info: console.info,
+    debug: console.debug,
+};
