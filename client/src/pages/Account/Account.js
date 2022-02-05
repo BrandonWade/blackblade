300a6ac61574e83fc17e732a1a4a9720c5008883
BrandonWade <brandon_wade2005@hotmail.com>
2022-02-04 18:14:07 -0600

    Begin implementing Change Password form.

--- client/src/pages/Account/Account.js
+++ client/src/pages/Account/Account.js
@@ -1,10 +1,70 @@
+import { useState } from 'react';
+import useValidation from '../../hooks/useValidation';
 import HeaderPage from '../../components/HeaderPage';
+import { PasswordInputField } from '../../components/PasswordInput';
+import { InputField } from '../../components/Input';
+import Button from '../../components/Button';
 import './Account.scss';
 
 export default function Account() {
+    const [currentPassword, setCurrentPassword] = useState('');
+    const [newPassword, setNewPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const { isPasswordLengthValid, doesPasswordContainValidChars, doPasswordsMatch } = useValidation();
+    const passwordLengthValid = isPasswordLengthValid(newPassword);
+    const passwordValidCharsOnly = doesPasswordContainValidChars(newPassword);
+    const passwordsMatch = doPasswordsMatch(newPassword, confirmPassword);
+    const isFormValid = passwordLengthValid && passwordValidCharsOnly && passwordsMatch;
+
+    const onChangeCurrentPassword = e => {
+        setCurrentPassword(e.target.value);
+    };
+
+    const onChangeNewPassword = e => {
+        setNewPassword(e.target.value);
+    };
+
+    const onChangeConfirmPassword = e => {
+        setConfirmPassword(e.target.value);
+    };
+
+    const onChangePassword = () => {
+        // TODO: Implement me
+    };
+
     return (
         <HeaderPage className='Account'>
-            <div>Account Page</div>
+            <div className='Account-content'>
+                <form className='Account-changePasswordForm' onSubmit={onChangePassword}>
+                    <InputField
+                        label='Current Password'
+                        rowClassName='Account-inputRow'
+                        labelClassName='Account-inputLabel'
+                        className='Account-input'
+                        value={currentPassword}
+                        onChange={onChangeCurrentPassword}
+                    />
+                    <PasswordInputField
+                        label='New Password'
+                        rowClassName='Account-inputRow'
+                        labelClassName='Account-inputLabel'
+                        className='Account-input'
+                        value={newPassword}
+                        onChange={onChangeNewPassword}
+                    />
+                    <PasswordInputField
+                        label='Confirm Password'
+                        rowClassName='Account-inputRow'
+                        labelClassName='Account-inputLabel'
+                        className='Account-input'
+                        value={confirmPassword}
+                        onChange={onChangeConfirmPassword}
+                    />
+                    <Button className='Account-changePasswordButton' type='submit' disabled={!isFormValid} onClick={onChangePassword}>
+                        Change Password
+                    </Button>
+                </form>
+            </div>
         </HeaderPage>
     );
 }
