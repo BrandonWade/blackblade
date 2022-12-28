0d48affa8f97e188d72473518b10408a3804bf66
BrandonWade <brandon_wade2005@hotmail.com>
2022-12-28 12:07:30 -0600

    Sort card rulings by publish date.

--- client/src/components/CardRulings/CardRulings.js
+++ client/src/components/CardRulings/CardRulings.js
@@ -2,9 +2,11 @@ import CardRuling from './CardRuling';
 import './CardRulings.scss';
 
 export default function CardRulings({ rulings = [] }) {
-    return rulings?.length > 0 ? (
+    const sortedRulings = rulings?.sort((a, b) => a.published_at.localeCompare(b.published_at)) || [];
+
+    return sortedRulings?.length > 0 ? (
         <div className='CardRulings'>
-            {rulings.map(ruling => {
+            {sortedRulings.map(ruling => {
                 return <CardRuling key={ruling.id} comment={ruling.comment} publishedAt={ruling.published_at} />;
             })}
         </div>
