4df83c2776a5026333a0948be4f283fbbdda61a4
BrandonWade <brandon_wade2005@hotmail.com>
2023-02-28 21:44:32 -0600

    Continue adding prop types and loading skeletons.

--- client/src/components/Checkbox/Checkbox.jsx
+++ client/src/components/Checkbox/Checkbox.jsx
@@ -1,6 +1,12 @@
+import PropTypes from 'prop-types';
+import LoadingSkeleton from '../LoadingSkeleton';
 import './Checkbox.scss';
 
-export default function Checkbox({ className = '', value = false, onClick = () => {}, children = [] }) {
+function Checkbox({ className = '', loading = false, children = [], value = false, onClick = () => {} }) {
+    if (loading) {
+        return <LoadingSkeleton className={`Checkbox--loading ${className}`} />;
+    }
+
     return (
         <label className={`Checkbox-label ${className}`} onClick={onClick}>
             <span className={`Checkbox-checkmark ${value ? 'Checkbox--checked' : ''}`} />
@@ -8,3 +14,13 @@ export default function Checkbox({ className = '', value = false, onClick = () =
         </label>
     );
 }
+
+Checkbox.propTypes = {
+    className: PropTypes.string,
+    loading: PropTypes.bool,
+    children: PropTypes.oneOfType([PropTypes.object, PropTypes.array, PropTypes.string]),
+    value: PropTypes.bool,
+    onClick: PropTypes.func,
+};
+
+export default Checkbox;
