b44cae2da5cb2c8b156ac747063382b9a5bac981
BrandonWade <brandon_wade2005@hotmail.com>
2023-03-08 22:11:07 -0600

    Add skeletons and prop types for Card Rulings.

--- client/src/components/CardRulings/CardRuling.jsx
+++ client/src/components/CardRulings/CardRuling.jsx
@@ -1,6 +1,17 @@
+import PropTypes from 'prop-types';
 import useSymbols from '../../hooks/useSymbols';
+import LoadingSkeleton from '../LoadingSkeleton';
 
-export default function CardRuling({ comment = '', publishedAt = '' }) {
+function CardRuling({ loading = false, comment = '', publishedAt = '' }) {
+    if (loading) {
+        return (
+            <div className='CardRulings-ruling'>
+                <LoadingSkeleton className='CardRulings-publishedAt--loading' />
+                <LoadingSkeleton className='CardRulings-comment--loading' />
+            </div>
+        );
+    }
+
     return (
         <div className='CardRulings-ruling'>
             <div className='CardRulings-publishedAt'>{publishedAt}</div>
@@ -8,3 +19,11 @@ export default function CardRuling({ comment = '', publishedAt = '' }) {
         </div>
     );
 }
+
+CardRuling.propType = {
+    loading: PropTypes.bool,
+    comment: PropTypes.string,
+    publishedAt: PropTypes.string,
+};
+
+export default CardRuling;
--- client/src/components/CardRulings/CardRulings.jsx
+++ client/src/components/CardRulings/CardRulings.jsx
@@ -1,17 +1,36 @@
+import PropTypes from 'prop-types';
 import CardSection from '../CardSection';
 import CardRuling from './CardRuling';
 import './CardRulings.scss';
 
-export default function CardRulings({ rulings = [] }) {
+function CardRulings({ loading = false, className = '', rulings = [] }) {
     const sortedRulings = rulings?.sort((a, b) => a.published_at.localeCompare(b.published_at)) || [];
 
-    return sortedRulings?.length > 0 ? (
-        <div className='CardRulings'>
-            <CardSection className='CardRulings-rulingsContainer'>
-                {sortedRulings.map(ruling => {
-                    return <CardRuling key={ruling.id} comment={ruling.comment} publishedAt={ruling.published_at} />;
-                })}
+    if (loading) {
+        return (
+            <CardSection className={`CardRulings ${className}`}>
+                <CardRuling loading={loading} />
+                <CardRuling loading={loading} />
+                <CardRuling loading={loading} />
+                <CardRuling loading={loading} />
+                <CardRuling loading={loading} />
             </CardSection>
-        </div>
+        );
+    }
+
+    return sortedRulings?.length > 0 ? (
+        <CardSection className={`CardRulings ${className}`}>
+            {sortedRulings.map(ruling => {
+                return <CardRuling key={ruling.id} comment={ruling.comment} publishedAt={ruling.published_at} />;
+            })}
+        </CardSection>
     ) : null;
 }
+
+CardRulings.propTypes = {
+    loading: PropTypes.bool,
+    className: PropTypes.string,
+    rulings: PropTypes.array,
+};
+
+export default CardRulings;
