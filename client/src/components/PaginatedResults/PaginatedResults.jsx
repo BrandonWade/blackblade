4639349525ea6550b668938ea31cc24636a0692c
BrandonWade <brandon_wade2005@hotmail.com>
2023-03-19 08:41:49 -0500

    Add propTypes and skeletons for PaginatedResults.

--- client/src/components/PaginatedResults/PaginatedResults.jsx
+++ client/src/components/PaginatedResults/PaginatedResults.jsx
@@ -1,11 +1,13 @@
+import PropTypes from 'prop-types';
 import useSearch from '../../hooks/useSearch';
 import useDisplayResults from '../../hooks/useDisplayResults';
 import Paginator from '../../components/Paginator';
 import CardGrid from '../CardGrid';
 import './PaginatedResults.scss';
 
-export default function PaginatedResults({
+function PaginatedResults({
     forwardRef = null,
+    loading = false,
     className = '',
     paginatorClassName = '',
     gridClassName = '',
@@ -85,13 +87,47 @@ export default function PaginatedResults({
     return (
         <div className={`PaginatedResults-content ${className}`}>
             <Paginator
+                loading={loading}
                 className={`PaginatedResults-paginator ${paginatorClassName}`}
                 totalResults={totalResults}
                 numberOfPages={numberOfPages}
                 page={page}
                 onPageChange={onPageChange}
             />
-            <CardGrid gridClassName={gridClassName} cards={searchResults} isLink={isLink} onClick={onSelectResult} />
+            <CardGrid loading={loading} gridClassName={gridClassName} cards={searchResults} isLink={isLink} onClick={onSelectResult} />
         </div>
     );
 }
+
+PaginatedResults.propTypes = {
+    forwardRef: PropTypes.object,
+    loading: PropTypes.bool,
+    className: PropTypes.string,
+    paginatorClassName: PropTypes.string,
+    gridClassName: PropTypes.string,
+    onSelectResult: PropTypes.func,
+    isLink: PropTypes.bool,
+    redirect: PropTypes.bool,
+    name: PropTypes.string,
+    text: PropTypes.string,
+    selectedTypes: PropTypes.array,
+    colors: PropTypes.object,
+    matchType: PropTypes.string,
+    selectedSets: PropTypes.array,
+    cmc: PropTypes.object,
+    power: PropTypes.object,
+    toughness: PropTypes.object,
+    loyalty: PropTypes.object,
+    rarities: PropTypes.object,
+    flavorText: PropTypes.string,
+    page: PropTypes.number,
+    setPage: PropTypes.func,
+    totalResults: PropTypes.number,
+    setTotalResults: PropTypes.func,
+    searchResults: PropTypes.array,
+    setSearchResults: PropTypes.func,
+    numberOfPages: PropTypes.number,
+    setNumberOfPages: PropTypes.func,
+};
+
+export default PaginatedResults;
